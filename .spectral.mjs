04f50f260ecbc92496a0d1c62b0e5c86830f71e7
kees@vanginkels.com <kees@vanginkels.com>
2025-09-29 16:06:08 +0200

    Modified 4 files

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://stoplight.io/api/v1/projects/cHJqOjI4Nzk2Nw/spectral.js?branch_slug=main&token=af1dcd09-b884-46a1-b373-fa51f9a4b58d";
+export default { extends: ruleset };
